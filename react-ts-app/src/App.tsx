ffe493916a4affbe406dd4dcb9aa1cead27c6517
ishigami <ishigami@joyworks.jp>
2023-12-20 10:19:14 +0900

    api key  以外の部分

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { Row } from './Row';
+import { requests } from './utils/request';
+import { Banner } from './Banner';
+import { Nav } from './Nav';
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Banner />
+          <Row
+      title="NETFLIX ORIGUINALS"
+      fetchUrl= {`https://api.themoviedb.org/3${requests.feachNetflixOriginals}`}
+      isLargeRow
+    />
+    <Row title="Top Rated" fetchUrl={`https://api.themoviedb.org/3${requests.feactTopRated}`} />
+    <Row title="Action Movies" fetchUrl={`https://api.themoviedb.org/3${requests.feactActionMovies}`} />
+    <Row title="Comedy Movies" fetchUrl={`https://api.themoviedb.org/3${requests.feactComedyMovies}`} />
+    <Row title="Horror Movies" fetchUrl={`https://api.themoviedb.org/3${requests.feactHorrorMovies}`} />
+    <Row title="Romance Movies" fetchUrl={`https://api.themoviedb.org/3${requests.feactRomanceMovies}`}/>
+    <Row title="DOcumentaries" fetchUrl={`https://api.themoviedb.org/3${requests.feactDocumentMovies}`} />
+
     </div>
   );
 }
